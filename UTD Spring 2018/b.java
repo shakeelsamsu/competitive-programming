f08513b98c64c1139e91f40d024cbe309ad690f2
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2018-10-12 17:42:37 -0500

    Update b.java
    figured out a simple formula without iteration

--- UTD Spring 2018/b.java
+++ UTD Spring 2018/b.java
@@ -8,12 +8,8 @@ public class b {
         int n = in.nextInt();
         for(int i = 0; i < n; i++) {
             System.out.print(in.nextInt() + " ");
-            System.out.println(chanukah(in.nextInt()));
+            int days = in.nextInt();
+            System.out.println(days * (days + 1)) / 2 + days);
         }
-    }
-    public static int chanukah(int days) {
-        int candles = days;
-        for(int i = 0; i < days; i++) candles += i + 1;
-        return candles;
     }
 }
